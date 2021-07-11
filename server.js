38e0b3a0e261aa73656de7a405bcf1876b1322b8
imjord <dean.jay.goat@gmail.com>
2021-07-11 13:28:45 -0600

    querys

--- server.js
+++ server.js
@@ -23,16 +23,44 @@ const db = mysql.createConnection(
     console.log('youre connected to the election database')
 );
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// })
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+})
 
 db.query('SELECT * FROM candidates', (err, rows) => {
     console.log(rows);
 })
 
+// get a single candidate 
+
+db.query(' SELECT * FROM candidates WHERE id =1', (err, row) => {
+    if(err){
+        console.log(err);
+    } 
+    console.log(row);
+})
+
+
+// delete a candidate 
+
+db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => { 
+    if(err){
+        console.log(err);
+    }
+    console.log(result);
+})
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql, params, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
 
 
 
